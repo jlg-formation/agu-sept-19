fa764210cda2d68908d7aa9ae98ec7334ccc9931
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-09-17 14:50:53 +0200

    reecriture en ts

---
+++ back/server.ts
@@ -0,0 +1,17 @@
+const express = require('express');
+const serveIndex = require('serve-index');
+
+
+const app = express();
+const port = 3000;
+const htdocs = '../front/dist/front/';
+
+app.use((req, res, next) => {
+    console.log('req.url', req.url);
+    next();
+});
+
+app.use(express.static(htdocs));
+app.use(serveIndex(htdocs, { icons: true }));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
