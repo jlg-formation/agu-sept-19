a7d6c7a8ca3c3fea769779a7aab1034a5dff2b86
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-09-18 14:53:30 +0200

    create finished component

--- front/src/app/create/create-routing.module.ts
+++ front/src/app/create/create-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateComponent } from './create/create.component';
 import { SetupComponent } from './setup/setup.component';
 import { AddQuestionComponent } from './add-question/add-question.component';
+import { FinishedComponent } from './finished/finished.component';
 
 
 const routes: Routes = [
   { path: 'create', component: CreateComponent },
   { path: 'setup', component: SetupComponent },
   { path: 'add-question', component: AddQuestionComponent },
+  { path: 'finished', component: FinishedComponent },
 ];
 
 @NgModule({
--- front/src/app/create/create.module.ts
+++ front/src/app/create/create.module.ts
@@ -5,10 +5,11 @@ import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create/create.component';
 import { SetupComponent } from './setup/setup.component';
 import { AddQuestionComponent } from './add-question/add-question.component';
+import { FinishedComponent } from './finished/finished.component';
 
 
 @NgModule({
-  declarations: [CreateComponent, SetupComponent, AddQuestionComponent],
+  declarations: [CreateComponent, SetupComponent, AddQuestionComponent, FinishedComponent],
   imports: [
     CommonModule,
     CreateRoutingModule
