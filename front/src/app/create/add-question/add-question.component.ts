4d09543f195eecfcae94d6d26a713c024172eb1b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-09-18 17:15:48 +0200

    add question

--- front/src/app/create/add-question/add-question.component.ts
+++ front/src/app/create/add-question/add-question.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-question',
@@ -8,7 +8,14 @@ import { FormGroup } from '@angular/forms';
 })
 export class AddQuestionComponent implements OnInit {
 
-  f = new FormGroup({});
+  f = new FormGroup({
+    label: new FormControl('Quelle est le nom de la capitale de la France ?', Validators.required),
+    answerA: new FormControl('Berlin', Validators.required),
+    answerB: new FormControl('Paris', Validators.required),
+    answerC: new FormControl('Londres', Validators.required),
+    answerD: new FormControl('Moscou', Validators.required),
+    correctAnswer: new FormControl('', Validators.required)
+  });
 
   constructor() { }
 
