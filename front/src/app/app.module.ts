57c6682878a909c4af5db24097183b286a35f41a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-09-18 15:06:30 +0200

    font awesome

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { LayoutModule } from './layout/layout.module';
@@ -16,6 +17,7 @@ import { CreateModule } from './create/create.module';
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule,
     LayoutModule,
     CreateModule
--- front/src/app/create/create.module.ts
+++ front/src/app/create/create.module.ts
@@ -6,13 +6,15 @@ import { CreateComponent } from './create/create.component';
 import { SetupComponent } from './setup/setup.component';
 import { AddQuestionComponent } from './add-question/add-question.component';
 import { FinishedComponent } from './finished/finished.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [CreateComponent, SetupComponent, AddQuestionComponent, FinishedComponent],
   imports: [
     CommonModule,
-    CreateRoutingModule
+    CreateRoutingModule,
+    FontAwesomeModule,
   ]
 })
 export class CreateModule { }
