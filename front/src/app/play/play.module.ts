3b660f10c7156cfddd911be1a815769e0b424d43
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-09-19 09:51:10 +0200

    create routes

--- front/src/app/play/play.module.ts
+++ front/src/app/play/play.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PlayRoutingModule } from './play-routing.module';
+import { ListComponent } from './list/list.component';
+import { QuestionComponent } from './question/question.component';
+import { ScoreComponent } from './score/score.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent, QuestionComponent, ScoreComponent],
   imports: [
     CommonModule,
     PlayRoutingModule
