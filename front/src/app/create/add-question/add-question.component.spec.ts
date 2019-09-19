745eaaf755ba3d1f84725b275e9416c37acceca4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-09-19 16:54:21 +0200

    ng test submit add question ok

--- front/src/app/create/add-question/add-question.component.spec.ts
+++ front/src/app/create/add-question/add-question.component.spec.ts
@@ -1,17 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { Location } from '@angular/common';
 
 import { AddQuestionComponent } from './add-question.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { QuizzService } from 'src/app/quizz.service';
+import { Quizz } from 'src/app/quizz';
+import { Routes } from '@angular/router';
+import { SetupComponent } from '../setup/setup.component';
 
 describe('AddQuestionComponent', () => {
   let component: AddQuestionComponent;
   let fixture: ComponentFixture<AddQuestionComponent>;
+  let location: Location;
+
+  const myQuizzService = new QuizzService();
+  const q = new Quizz();
+  q.name = 'toto';
+  myQuizzService.setCurrent(q);
+
+  const routes: Routes = [
+    { path: 'setup', component: SetupComponent },
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, RouterTestingModule],
-      declarations: [AddQuestionComponent]
+      imports: [ReactiveFormsModule, RouterTestingModule.withRoutes(routes)],
+      declarations: [AddQuestionComponent, SetupComponent],
+      providers: [{
+        provide: QuizzService, useValue: myQuizzService
+      }]
     })
       .compileComponents();
   }));
@@ -20,9 +38,18 @@ describe('AddQuestionComponent', () => {
     fixture = TestBed.createComponent(AddQuestionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    location = TestBed.get(Location);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should submit', fakeAsync(() => {
+    const nbr = myQuizzService.current.questions.length;
+    component.submit();
+    expect(myQuizzService.current.questions.length).toBe(nbr + 1);
+    tick();
+    expect(location.path()).toBe('/setup');
+  }));
 });
