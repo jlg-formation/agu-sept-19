8addaa8b507de61f12fc0ad8867b2be8926ec4f0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-09-20 15:01:14 +0200

    button radio relooking

--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AutofocusDirective } from './autofocus.directive';
 import { TimerComponent } from './timer/timer.component';
 import { TimeFormatPipe } from './time-format.pipe';
+import { RadioButtonComponent } from './radio-button/radio-button.component';
 
 
 
 @NgModule({
-  declarations: [AutofocusDirective, TimerComponent, TimeFormatPipe],
+  declarations: [AutofocusDirective, TimerComponent, TimeFormatPipe, RadioButtonComponent],
   imports: [
     CommonModule
   ],
-  exports: [AutofocusDirective, TimerComponent, TimeFormatPipe]
+  exports: [AutofocusDirective, TimerComponent, TimeFormatPipe, RadioButtonComponent]
 })
 export class WidgetModule { }
